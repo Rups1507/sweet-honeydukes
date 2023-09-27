372a794f63a349cfa688037efd95cccb29c50e18
Rups1507 <rupesh11tiwari00@gmail.com>
2023-09-27 23:34:43 +0530

    FrontEnD and BackEnd

---
+++ BackEnd/Sweet_HoneyDukes/src/main/java/com/masai/SweetHoneyDukesApplication.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetHoneyDukesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetHoneyDukesApplication.class, args);
+	}
+
+}
