771c42e1ffb514070b808cfa3da57c4c89e960e2
Rups1507 <rupesh11tiwari00@gmail.com>
2023-10-02 02:20:40 +0530

    AppConfig patrial

--- BackEnd/Sweet_HoneyDukes/src/main/java/com/masai/config/AppConfig.java
+++ BackEnd/Sweet_HoneyDukes/src/main/java/com/masai/config/AppConfig.java
@@ -1,10 +1,11 @@
 package com.masai.config;
 
-import java.beans.Customizer;
+
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
