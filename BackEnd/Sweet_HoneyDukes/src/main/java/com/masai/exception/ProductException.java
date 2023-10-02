e3b77d891094a3979199c46035b4448c184f2adb
Rups1507 <rupesh11tiwari00@gmail.com>
2023-10-02 15:27:39 +0530

    Global Exception Handler

--- BackEnd/Sweet_HoneyDukes/src/main/java/com/masai/exception/ProductException.java
+++ BackEnd/Sweet_HoneyDukes/src/main/java/com/masai/exception/ProductException.java
@@ -1,6 +1,6 @@
 package com.masai.exception;
 
-public class ProductException extends Exception {
+public class ProductException extends RuntimeException {
 
 	public ProductException(String message) {
 		super(message);
