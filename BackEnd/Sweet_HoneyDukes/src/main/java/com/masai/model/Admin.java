287b2646b6a74933656c3a542af633b426070dd1
Yogesh Sorde <115467270+YogeshSorde@users.noreply.github.com>
2023-10-02 14:46:50 +0530

    controller done

--- BackEnd/Sweet_HoneyDukes/src/main/java/com/masai/model/Admin.java
+++ BackEnd/Sweet_HoneyDukes/src/main/java/com/masai/model/Admin.java
@@ -3,11 +3,11 @@ package com.masai.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -18,19 +18,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 @Entity
-
+@DiscriminatorValue("Admin")
 public class Admin extends User {
-  @Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int adminId;
-	@NotNull(message = "Name of admin can't be Null")
-	private String name;
-	@NotNull(message = "Email of admin can't be Null")
-	@Email(message = "Email should be in proper Format")
-	@Column(unique = true)
-	private String email;
-	@NotNull(message = "Password of admin can't be Null")
-	private String password;
+  
 	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
 	private boolean active=true;
 	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
